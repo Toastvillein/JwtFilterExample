109fd82256d8bf8ee385e34d2d64b4a0f085a1a6
Toastvillein <okkcba7899@gmail.com>
2025-05-03 00:45:41 +0900

    first commit

---
+++ src/main/java/com/example/jwtfilterpractice/JwtFilterPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtfilterpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtFilterPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtFilterPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtfilterpractice/JwtFilterPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtfilterpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtFilterPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
